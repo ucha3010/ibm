1746de1ea294234b89084b4332be165c0747c3aa
Damian <damianjava@gmail.com>
2022-09-19 12:31:07 +0200

    subida inicial

---
+++ src/main/java/com/damian/testibm/TestibmApplication.java
@@ -0,0 +1,13 @@
+package com.damian.testibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestibmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestibmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damian/testibm/TestibmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damian.testibm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestibmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
